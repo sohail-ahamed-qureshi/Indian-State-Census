99299401171abdf86651d2f68c224a8559f3b56f
sohail <sohailqureshi82@gmail.com>
2021-06-28 14:19:05 +0530

    [soahil] Indian state census problem statement

---
+++ IndianStateCensus/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IndianStateCensus
+{
+    class Program
+    {
+        /// <summary>
+        /// Indian State census problem statement..
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("India State Census Problem statement");
+        }
+    }
+}
