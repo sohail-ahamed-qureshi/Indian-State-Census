e1cb11f6bf36b95ef2bde627d88135e637c5aed6
sohail <sohailqureshi82@gmail.com>
2021-07-05 03:51:30 +0530

    [sohail] Add: incorect file passed gives custom exception

--- NunitIndianStateCensus.Test/StateCensusTestCases.cs
+++ NunitIndianStateCensus.Test/StateCensusTestCases.cs
@@ -6,7 +6,7 @@ namespace NunitIndianStateCensus.Test
     public class Tests
     {
         string indianStateHeaders = "State,Population,AreaInSqKm,DensityPerSqKm";
-        string indianStateCensusfilepath = @"C:\Users\Admin\Desktop\BridgeLabs Assignments\Indian-State-Census\IndianStateCensus\Indian state census data\IndiaStateCensusData.csv";
+        string indianStateCensusfilepath = @"C:\Users\Admin\Desktop\BridgeLabs Assignments\Indian-State-Census\IndianStateCensus\Indian state census data\IndiaState.csv";
 
 
         [Test]
@@ -14,6 +14,18 @@ namespace NunitIndianStateCensus.Test
         {
             //Arrange
             int expected = 29;
+            //Act
+            StateCensus stateCensus = new StateCensus();
+            int totalRecords = stateCensus.GetData(indianStateCensusfilepath, indianStateHeaders);
+            //Assert
+            Assert.AreEqual(expected, totalRecords);
+        }
+
+        [Test]
+        public void GivenCSVfileIncorrect_ReturnsCustomException()
+        {
+            //Arrange
+            int expected = 0;
             //Act
             StateCensus stateCensus = new StateCensus();
             int totalRecords = stateCensus.GetData(indianStateCensusfilepath, indianStateHeaders);
