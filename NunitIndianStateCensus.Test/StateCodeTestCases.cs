680784e7bb5740c9f194a53511a87fe16522c65d
sohail <sohailqureshi82@gmail.com>
2021-07-05 04:28:25 +0530

    [sohail] Add: incorrect filetype passed gives custom exception

--- NunitIndianStateCensus.Test/StateCodeTestCases.cs
+++ NunitIndianStateCensus.Test/StateCodeTestCases.cs
@@ -9,7 +9,9 @@ namespace NunitIndianStateCensus.Test
         string indianStateCodeHeaders = "SrNo,State Name,TIN,StateCode";
         string indianStateCodefilepath = @"C:\Users\Admin\Desktop\BridgeLabs Assignments\Indian-State-Census\IndianStateCensus\Indian state census data\IndiaStateCode.csv";
         string wrongfile = @"C:\Users\Admin\Desktop\BridgeLabs Assignments\Indian-State-Census\IndianStateCensus\Indian state census data\StateCode.csv";
-        [Test]
+        string wrongfiletype = @"C:\Users\Admin\Desktop\BridgeLabs Assignments\Indian-State-Census\IndianStateCensus\Indian state census data\IndiaStateCode.txt";
+
+         [Test]
         public void GivenCSVfile_ReturnsNumberOfRecord()
         {
             //Arrange
@@ -29,6 +31,18 @@ namespace NunitIndianStateCensus.Test
             //Act
             StateCensusAnalyser stateCensus = new StateCensusAnalyser();
             int totalRecords = stateCensus.GetStateCodeData(wrongfile, indianStateCodeHeaders);
+            //Assert
+            Assert.AreEqual(expected, totalRecords);
+        }
+
+        [Test]
+        public void GivenIncorrectFiletype_ReturnsCustomException()
+        {
+            //Arrange
+            int expected = 0;
+            //Act
+            StateCensusAnalyser stateCensus = new StateCensusAnalyser();
+            int totalRecords = stateCensus.GetStateCodeData(wrongfiletype, indianStateCodeHeaders);
             //Assert
             Assert.AreEqual(expected, totalRecords);
         }
