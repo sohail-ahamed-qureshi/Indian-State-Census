945d4b7cb7bdbbad9d44ef20b65f5208e0c68fb3
sohail <sohailqureshi82@gmail.com>
2021-07-05 04:25:26 +0530

    [sohail] Add: given incorrect file returns custom exception

--- NunitIndianStateCensus.Test/StateCodeTestCases.cs
+++ NunitIndianStateCensus.Test/StateCodeTestCases.cs
@@ -7,8 +7,8 @@ namespace NunitIndianStateCensus.Test
     class StateCodeTestCases
     {
         string indianStateCodeHeaders = "SrNo,State Name,TIN,StateCode";
-        string indianStateCodefilepath = @"C:\Users\Admin\Desktop\BridgeLabs Assignments\Indian-State-Census\IndianStateCensus\Indian state census data\IndiaStateCode.csv"; 
-
+        string indianStateCodefilepath = @"C:\Users\Admin\Desktop\BridgeLabs Assignments\Indian-State-Census\IndianStateCensus\Indian state census data\IndiaStateCode.csv";
+        string wrongfile = @"C:\Users\Admin\Desktop\BridgeLabs Assignments\Indian-State-Census\IndianStateCensus\Indian state census data\StateCode.csv";
         [Test]
         public void GivenCSVfile_ReturnsNumberOfRecord()
         {
@@ -17,6 +17,18 @@ namespace NunitIndianStateCensus.Test
             //Act
             StateCensusAnalyser stateCensus = new StateCensusAnalyser();
             int totalRecords = stateCensus.GetStateCodeData(indianStateCodefilepath, indianStateCodeHeaders);
+            //Assert
+            Assert.AreEqual(expected, totalRecords);
+        }
+
+        [Test]
+        public void GivenCSVfileIncorrect_ReturnsCustomException()
+        {
+            //Arrange
+            int expected = 0;
+            //Act
+            StateCensusAnalyser stateCensus = new StateCensusAnalyser();
+            int totalRecords = stateCensus.GetStateCodeData(wrongfile, indianStateCodeHeaders);
             //Assert
             Assert.AreEqual(expected, totalRecords);
         }
