18a9644f41fd40dd7633abb669e04a94933228bf
sohail <sohailqureshi82@gmail.com>
2021-07-05 04:21:42 +0530

    [sohail] Add: check fot total number of records

---
+++ NunitIndianStateCensus.Test/StateCodeTestCases.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using IndianStateCensus;
+using NUnit.Framework;
+
+namespace NunitIndianStateCensus.Test
+{
+    class StateCodeTestCases
+    {
+        string indianStateCodeHeaders = "SrNo,State Name,TIN,StateCode";
+        string indianStateCodefilepath = @"C:\Users\Admin\Desktop\BridgeLabs Assignments\Indian-State-Census\IndianStateCensus\Indian state census data\IndiaStateCode.csv"; 
+
+        [Test]
+        public void GivenCSVfile_ReturnsNumberOfRecord()
+        {
+            //Arrange
+            int expected = 37;
+            //Act
+            StateCensusAnalyser stateCensus = new StateCensusAnalyser();
+            int totalRecords = stateCensus.GetStateCodeData(indianStateCodefilepath, indianStateCodeHeaders);
+            //Assert
+            Assert.AreEqual(expected, totalRecords);
+        }
+    }
+}
