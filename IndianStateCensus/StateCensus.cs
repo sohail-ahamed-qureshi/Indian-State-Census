9fb913850507058accc06449bfe16c0b7a00e140
sohail <sohailqureshi82@gmail.com>
2021-07-05 00:23:01 +0530

    [sohail] Add: Ability to load the analyser to load state census data feom csv file

---
+++ IndianStateCensus/StateCensus.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace IndianStateCensus
+{
+    class StateCensus
+    {
+        public string State { get; set; }
+        public long Population { get; set; }
+        public int Area { get; set; }
+        public int Density { get; set; }
+        /// <summary>
+        /// loads the data from the csv file into census model class
+        /// </summary>
+        public int GetData()
+        {
+            string filepath = @"C:\Users\Admin\Desktop\BridgeLabs Assignments\Indian-State-Census\IndianStateCensus\Indian state census data\IndiaStateCensusData.csv";
+            string[] data = File.ReadAllLines(filepath);
+            var censusList = new List<StateCensus>();
+            //iterator to load the data
+            for (int i = 1; i < data.Length; i++)
+            {
+                string[] censusData = data[i].Split(',');
+
+                State = censusData[0];
+                Population = Convert.ToInt64(censusData[1]);
+                Area = Convert.ToInt32(censusData[2]);
+                Density = Convert.ToInt32(censusData[3]);
+
+                Console.WriteLine($"State : {State} Population: {Population} Area: {Area} Density: {Density}");
+
+            }
+            return data.Length;
+        }
+    }
+}
---
+++ IndianStateCensus/StateCensusAnalyser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace IndianStateCensus
+{
+    class StateCensusAnalyser
+    {
+        /// <summary>
+        /// loads the data directly from the csv file
+        /// </summary>
+        public int GetData()
+        {
+            string filepath = @"C:\Users\Admin\Desktop\BridgeLabs Assignments\Indian-State-Census\IndianStateCensus\Indian state census data\IndiaStateCensusData.csv";
+            string[] data = File.ReadAllLines(filepath);
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.WriteLine(data[i]);
+            }
+            return data.Length;
+        }
+    }
+}
